e2552832f66dea64404aee35be84698d60006c47
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-10-25 21:34:34 +0300

    Add files via upload

---
+++ exe2.15_3.py
@@ -0,0 +1,17 @@
+'''Exercise 4: Assume that we execute the following assignment statements:
+width = 17
+height = 12.0
+For each of the following expressions, write the value of the expression and the
+type (of the value of the expression).
+1. width//2
+2. width/2.0
+3. height/3
+4. 1 + 2 * 5
+'''
+width  = 17
+height = 12.0
+
+print("Width//2 : {}, type {}".format(width//2, type(width//2)))
+print("width/2.0: {}, type {}".format(width/2.0, type(width/2.0)))
+print("height/3: {}, type {}".format(height/3, type(height/3)))
+print("1+2*5: {}, type {}".format(1+2*5, type(1+2*5)))
