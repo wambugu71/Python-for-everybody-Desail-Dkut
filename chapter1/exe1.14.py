927131939c6870d17c35829339fcc2e6b55ed14a
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2023-10-26 15:18:55 +0300

    Create exe1.14.py

---
+++ chapter1/exe1.14.py
@@ -0,0 +1,45 @@
+'''' 1.14 Exercises
+Exercise 1: What is the function of the secondary memory in a computer?
+a) Execute all of the computation and logic of the program
+b) Retrieve web pages over the Internet
+c) Store information for the long term, even beyond a power cycle
+d) Take input from the user
+Answer: C Store information for the long term, even beyond a power cycle
+''''
+'''
+Aprogram is a set of instructions written in a programming  language that are executed by   a computer  '''
+'''
+Exercise 3: A compiler is a program that translates source code written in a high-level programming 
+language into machine code that can be understood and executed directly by the computer's processor.
+An interpreter, on the other hand, reads and executes the source code line by line without first compiling i
+t into machine code. In other words, a compiler converts the entire program into machine code before running it, 
+while an interpreter interprets the program one instruction at a time and performs the corresponding actions.
+'''
+''' A word processing  document '''
+''' Primt instead of  print '''
+''' Main memory '''
+'''42 '''
+''' Central processing  Unit  - IN Humans thats the brain, its the brain of  a  computer. Executes  instructions and calculations
+Main memory - in humans it  is the  short-term memory. it  holds the information briefly  as wework on it.
+Secondary memory  stores long-term information.
+Input  devices - In humans represents  the would be our senses on how we persive the world which allow us to perceive the world around us and send signals to our brains. 
+For example, just as a keyboard inputs text into a computer '''
+''' Output devices -  human equivalent of an output device would be our muscles,
+which carry out physical actions based on signals from the brain. For example, 
+just as a printer outputs a hard copy of a document.
+'''
+
+''''
+reading Examine your code, read it back to yourself, and check that it says what
+you meant to say.
+running Experiment by making changes and running different versions. Often
+if you display the right thing at the right place in the program, the problem becomes obvious, but sometimes you have to spend some time to build
+scaffolding.
+ruminating Take some time to think! What kind of error is it: syntax, runtime,
+semantic? What information can you get from the error messages, or from
+the output of the program? What kind of error could cause the problem
+you’re seeing? What did you change last, before the problem appeared?
+retreating At some point, the best thing to do is back off, undoing recent changes,
+until you get back to a program that works and that you understand. Then
+you can start rebuilding. 
+''''
